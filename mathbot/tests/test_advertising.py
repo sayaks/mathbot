25ad96f3d34018c330ac01e7986f1b12a33ac333
sayaks <sayaks1@gmail.com>
2018-01-29 12:04:09 +0100

    made setup and teardown of parameters into a fixture

--- mathbot/tests/test_advertising.py
+++ mathbot/tests/test_advertising.py
@@ -1,39 +1,37 @@
 import advertising
-import automata
-import core.parameters
 import core.keystore
 import patrons
 import pytest
 
 import conftest
 
-async def test_no_advertise():
+@pytest.fixture(scope = 'function')
+def parameters():
+    import core.parameters
+    sources_bak = core.parameters.sources
+    core.parameters.reset()
+    yield core.parameters
+
+    # HACK: there isn't an idiomatic way of doing this yet    
+    core.parameters.reset(False)
+    core.parameters.sources = sources_bak
+
+async def test_no_advertise(parameters):
     '''
     This test checks that ads will not be displayed if the advertising
     key is set to False.
     '''
-    sources_bak = core.parameters.sources
-    core.parameters.reset()
-    core.parameters.add_source({'advertising': {'enable': False}})
-
+    parameters.add_source({'advertising': {'enable': False}})
     assert(not await advertising.should_advertise_to(0, 0))
-
-    # HACK: there isn't an idiomatic way of doing this yet
-    core.parameters.reset(False)
-    core.parameters.sources = sources_bak
 
 
 @conftest.automata_test
-async def test_advertise(interface):
-    sources_bak = core.parameters.sources
-    core.parameters.reset()
-    core.parameters.add_source({'advertising': {'enable': True}})
+async def test_advertise(interface, parameters):
+    parameters.add_source({'advertising': {'enable': True}})
     core.keystore.set('advert_counter', interface.channel, core.parameters.get('advertising interval') + 1)
     patrons.PATRONS = {interface.client: patrons.TIER_NONE}
 
     assert(await advertising.should_advertise_to(interface.client, interface.channel))
 
-    # HACK: there isn't an idiomatic way of doing this yet
-    core.parameters.reset(False)
-    core.parameters.sources = sources_bak
+    # HACK: no good way of doing this as of rn
     patrons.PATRONS = {}
