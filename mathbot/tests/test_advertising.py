6865265839368e36eadfa83159cdccc5ec467662
sayaks <sayaks1@gmail.com>
2018-01-29 18:50:16 +0100

    made test_advertise work

--- mathbot/tests/test_advertising.py
+++ mathbot/tests/test_advertising.py
@@ -1,37 +1,40 @@
 import advertising
 import core.keystore
+import core.parameters
 import patrons
 import pytest
 
 import conftest
 
-@pytest.fixture(scope = 'function')
-def parameters():
-    import core.parameters
+@pytest.fixture(scope = 'function', autouse=True)
+def parameters(request):
     sources_bak = core.parameters.sources
     core.parameters.reset()
     yield core.parameters
 
-    # HACK: there isn't an idiomatic way of doing this yet    
+    # HACK: there isn't an idiomatic way of doing this yet
     core.parameters.reset(False)
     core.parameters.sources = sources_bak
 
+@pytest.mark.asyncio
 async def test_no_advertise(parameters):
     '''
     This test checks that ads will not be displayed if the advertising
     key is set to False.
     '''
     parameters.add_source({'advertising': {'enable': False}})
-    assert(not await advertising.should_advertise_to(0, 0))
-
+    assert(not await advertising.should_advertise_to(None, None))
 
 @conftest.automata_test
-async def test_advertise(interface, parameters):
-    parameters.add_source({'advertising': {'enable': True}})
-    core.keystore.set('advert_counter', interface.channel, core.parameters.get('advertising interval') + 1)
-    patrons.PATRONS = {interface.client: patrons.TIER_NONE}
+async def test_advertise(interface):
+    core.parameters.reset()
+    core.parameters.add_source({'advertising': {'enable': True}})
+    keys_bak = await core.keystore.get('advert_counter')
+    await core.keystore.set('advert_counter', interface.channel.id, core.parameters.get('advertising interval') + 1)
+    patrons.PATRONS = {interface.target.id: patrons.TIER_NONE}
 
-    assert(await advertising.should_advertise_to(interface.client, interface.channel))
+    assert(await advertising.should_advertise_to(interface.target, interface.channel))
 
     # HACK: no good way of doing this as of rn
     patrons.PATRONS = {}
+    await core.keystore.set('advert_counter', interface.channel.id, keys_bak)
